b2eee13a3d6dc1b2c670cabf601c76e7fe033b6b
Diego Lopes <diego.silva@control.eng.br>
2019-05-31 09:51:20 -0300

    login app studio

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,48 @@
 import 'package:app_studio/pages/home_page.dart';
-import 'package:app_studio/pages/login_page.dart';
+//import 'package:app_studio/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:app_studio/pages/login_screen.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
 
-  final routes = <String, WidgetBuilder>{
+  /*final routes = <String, WidgetBuilder>{
     LoginPage.tag: (context) => LoginPage(),
     HomePage.tag: (context) => HomePage(),
-  };
+  };*/
 
   @override
   Widget build(BuildContext context) {
   return MaterialApp(
-      title: 'App',
+      title: 'AppStudio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        fontFamily: 'Nunito',
+        primarySwatch: Colors.lightBlue
       ),
       home: LoginPage(),
-      routes: routes,
+      //routes: routes,
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+	@override
+	 _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			backgroundColor: Colors.white,
+			body: new SingleChildScrollView(
+				 child: LoginScreen3()
+			),
+		);
+	}		
+}		
+		
+
 
